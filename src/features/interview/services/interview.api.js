1fdaf8be2746fbf1b6ad3a82c20444060bdac5e7
Yas Patle <you@example.com>
2026-03-29 22:58:08 +0530

    back

--- src/features/interview/services/interview.api.js
+++ src/features/interview/services/interview.api.js
@@ -1,10 +1,11 @@
 import axios from "axios";
 
-const api = axios.create({
-    baseURL: "http://localhost:3000",
-    withCredentials: true,
-})
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
+const api = axios.create({
+  baseURL: API_BASE_URL,
+  withCredentials: true
+});
 
 /**
  * @description Service to generate interview report based on user self description, resume and job description.
